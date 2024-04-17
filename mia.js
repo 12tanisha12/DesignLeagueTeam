e469653bbb0c3a5b3edeee75c748e1dfdb6865c8
m pineda <144722367+miap92@users.noreply.github.com>
2024-04-17 13:37:36 -0400

    Update mia.js

--- mia.js
+++ mia.js
@@ -1,1 +1,18 @@
+//loader
+window.addEventListener('load', () => {
+  const loader = document.querySelector('.loader');
+  setTimeout(() => {
+    loader.setAttribute('style', 'opacity: 0;');
+  }, 500);
+  setTimeout(() => {
+    loader.parentNode.removeChild(loader);
+    document.body.style.overflow = 'initial';
+  }, 1000);
+});
+
+
+
+
+
+
 
