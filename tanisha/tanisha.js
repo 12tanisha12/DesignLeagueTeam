a29309b6b89e8788c9472304307df382134109a0
12tanisha12 <163028909+12tanisha12@users.noreply.github.com>
2024-05-13 13:41:22 -0400

    Update tanisha.js

--- tanisha/tanisha.js
+++ tanisha/tanisha.js
@@ -3,7 +3,7 @@ window. addEventListener('load', () => {
   const loader = document.querySelector('.loader');
   setTimeout(() => {
     loader.setAttribute('style, 'opacity: 0;');
-  }, 500);
+  }, 500); 
   setTimeout(() => {
     loader.parentNode.removeChild(loader);
     document.body.style.overflow = 'initial';
